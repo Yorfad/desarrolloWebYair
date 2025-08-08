86435e2586c3fb864ec22e1aeeab61679d9e0026
Yorfad <alexmej267@gmail.com>
2025-08-08 12:22:47 -0600

    tienda online responsiva

--- script/script.js
+++ script/script.js
@@ -1,7 +1,48 @@
 let contador = 0;
+
+
 document.querySelectorAll('.btn-buy').forEach(btn => {
   btn.addEventListener('click', () => {
     contador++;
     document.getElementById('contador-carrito').textContent = contador;
   });
 });
+
+const contenedor = document.getElementById("container-cards");
+const template = document.getElementById("card");
+const fragmento = document.createDocumentFragment();
+
+
+async function cargarProductosDesdeAPI() {
+  try {
+    const respuesta = await fetch("https://backcvbgtmdesa.azurewebsites.net/api/productos"); // ✅ tu URL real aquí
+    const productos = await respuesta.json();
+
+    productos.forEach(producto => {
+      const clon = template.content.cloneNode(true);
+
+      clon.querySelector(".img").src = producto.Imagen;
+      clon.querySelector(".img").alt = producto.Nombre;
+      clon.querySelector(".title").textContent = producto.Nombre;
+      clon.querySelector(".descripction").textContent = producto.Descripcion;
+      clon.querySelector(".price").textContent = '$' + producto.Precio;
+      if(producto.EnOferta === true) {
+        clon.querySelector(".price").classList.add("text-danger");
+        clon.querySelector(".price-in-discount").textContent = '$' + producto.PrecioOferta
+      }
+
+      clon.querySelector(".btn-buy").addEventListener("click", () => {
+        alert(`Compraste: ${producto.Nombre}`);
+      });
+
+      fragmento.appendChild(clon);
+    });
+
+    contenedor.appendChild(fragmento);
+  } catch (error) {
+    console.error("Error al cargar los productos:", error);
+    contenedor.innerHTML = `<p class="text-danger">Ocurrió un error al cargar los productos.</p>`;
+  }
+}
+
+cargarProductosDesdeAPI();
